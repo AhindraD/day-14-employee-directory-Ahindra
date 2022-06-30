b173bbb02e647ac030b06a03702f32de0b26e5d9
Ahindra D <official.ahindra@gmail.com>
2022-06-30 12:16:30 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = 'Ahindra';
+let sayHello = () => 'Hey, There...';
+
 function App() {
+  let name = 'Ahindra';
+  let sayHello = () => 'Hey, There';
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, {name}
         </p>
         <a
           className="App-link"
@@ -15,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {sayHello()}
         </a>
       </header>
     </div>
