40ed6c6ff504d969243ddb7c881bfca2174e6d6b
Ahindra D <official.ahindra@gmail.com>
2022-06-30 14:47:14 +0530

    lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import UserCards from './components/UserCards';
 import users from "./users";
 
 function App() {
-  let name = 'Ahin';
-  let sayHello = () => 'Hey, There';
-
-
   return (
     <>
       <h1>Users at Acme Inc.</h1>
